2ef761afb9dbc74639c67a104e7b2ff96389c415
Parbelaez <132989779+Parbelaez@users.noreply.github.com>
2023-07-30 21:43:27 +0200

    Complete set of functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -8,11 +8,28 @@ function addTwoNumbers(a, b) {
     // Code block
     return a + b;
 }
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Arrow Function With Parameters
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers_arrow = (a, b) => a + b;
+let sum_arrow = addTwoNumbers_arrow(3, 5);
+console.log(sum_arrow);
+
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething("Hello there!");
+
+const sayHello = () => console.log("hello");
 
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+        This is a multiple line string!
+    </p>`
+);
+console.log(returnMultipleLines());
